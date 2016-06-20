742ff44c2a537c7f392304314871ad64396be1ef
martyhu <marty.hu@gmail.com>
2016-06-20 13:45:52 -0700

    add fish key binding

--- config.fish
+++ config.fish
@@ -3,5 +3,14 @@ function fish_greeting
     env ESV_KEY=2a3641d171cfa6eb devotion
 end
 
+function fish_user_key_bindings
+    bind -M insert \cr re-search
+end
+
 # set fish colors
 set -g TERM xterm-256color
+
+# Set path variable
+set -gx PATH ~/hacks/* $PATH
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
